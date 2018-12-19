7ff16979b664b70928f34d37242219aecb9a53d0
ChidiNwaka <chidi.nwaka@gmail.com>
2018-12-19 11:51:40 -0500

    Added cycleDetection solution.

---
+++ dataStructures/linkedList/cycleDetection.java
@@ -0,0 +1,27 @@
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static boolean hasCycle(SinglyLinkedListNode head) {
+
+        if (head == null) return false;
+
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = head; 
+
+        while ((fast.next != null) && (fast.next.next != null)){
+            
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast ) return true;
+        }
+
+        return false;
+    }
+
